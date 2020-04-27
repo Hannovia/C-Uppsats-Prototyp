a025412a95d25cb5f28a760231f99fc572d98775
Hannovia <33325512+Hannovia@users.noreply.github.com>
2020-04-27 14:33:40 +0200

    börjar om lite på menyn

--- Prototyp/Assets/Scripts/CanvasScript.cs
+++ Prototyp/Assets/Scripts/CanvasScript.cs
@@ -1,12 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CanvasScript : MonoBehaviour
 {
 
     public GameObject menu;
     private bool isShowing;
+
+    public Vector2 moveInput;
+
+    public Text[] options;
+
+    public Color normalColor, highlightedColor;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +24,40 @@ public class CanvasScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("escape"))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
-            Debug.Log("hejhej");
             isShowing = !isShowing;
             menu.SetActive(isShowing);
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            // Do something
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            // Do something
+        }
+
+        if (menu.activeInHierarchy)
+        {
+            moveInput.x = Input.mousePosition.x - (Screen.width / 2f);
+            moveInput.y = Input.mousePosition.y - (Screen.width / 2f);
+
+            moveInput.Normalize();
+
+            if (moveInput != Vector2.zero){
+                float angle = Mathf.Atan2(moveInput.y, -moveInput.x) / Mathf.PI;
+                angle *= 180;
+                angle += 90f;
+                if (angle < 0)
+                {
+                    angle += 360;
+                }
+
+                Debug.Log(angle);
+            }
         }
     }
 }
